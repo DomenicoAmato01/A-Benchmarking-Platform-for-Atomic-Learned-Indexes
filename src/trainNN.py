008b167594fcd06d960430ae3543301cb1af934b
DomenicoAmato01 <domenico.amato01@unipa.it>
2019-10-11 10:34:37 +0200

    Fixed trainNN error

--- src/trainNN.py
+++ src/trainNN.py
@@ -30,11 +30,11 @@ def max_error(set_dim):
 
 #Set Argument Parser for input information
 parser = argparse.ArgumentParser(description='Train NN for Datastructure index')
-parser.add_argument('-i', '--input', dest="inputFile", default="input.h5",
+parser.add_argument('-i', '--input', dest="inputFile", default="uni01.sorted.bin.h5",
                     help='Input file name')
-parser.add_argument('-id', '--inputDir', dest="inputDir", default="input.h5",
+parser.add_argument('-id', '--inputDir', dest="inputDir", default="rsc/uniform/",
 help='Input file path')
-parser.add_argument('-o', '--output', dest="outputPath", default="output.h5",
+parser.add_argument('-o', '--output', dest="outputPath", default="res/uniform/",
                     help='Output file path')
 parser.add_argument('-m', '--model', dest="models", nargs="+", default="1", type=int, choices=range(1, 4),
                     help='Model numbers: 1 -> No hidden Layer(Perceptron); 2 -> One hidden layer with 256 units; 3 -> Two hidden layer with 256 units')
@@ -53,7 +53,7 @@ print(params)
 
 #Load Dataset
 bin_data=[]
-with h5py.File(args.inputDir+"/"+args.inputFile,'r') as f:
+with h5py.File(args.inputDir+args.inputFile,'r') as f:
     data = f.get('Sb') 
     bin_data = np.array(data, dtype=np.bool) # For converting to numpy array
 bin_data = np.transpose(bin_data)
@@ -98,12 +98,14 @@ for model in args.models:
         nn_model.build()
         nn_model.summary()
         nn_name="NN3"
+    else:
+        raise "No model Found"
         
 
     #Callback for Training
     stopping = tf.keras.callbacks.EarlyStopping(monitor=params["monitor"], patience=params["patience"], verbose=params["verbose"])
-    chkpoints = tf.keras.callbacks.ModelCheckpoint(filepath=args.outputPath+"/"+params["chk-dir"]+"/"+nn_name+"/best_model.h5py", monitor=params["monitor"], verbose=params["verbose"], save_best_only=True, save_weights_only=True, mode='auto', period=1)
-    tensorboard = tf.keras.callbacks.TensorBoard(log_dir=args.outputPath+"/"+params["log-dir"]+"/"+nn_name, histogram_freq=None, embeddings_freq=None)
+    chkpoints = tf.keras.callbacks.ModelCheckpoint(filepath=args.outputPath+params["chk-dir"]+"/"+nn_name+"/best_model.h5py", monitor=params["monitor"], verbose=params["verbose"], save_best_only=True, save_weights_only=True, mode='auto', period=1)
+    tensorboard = tf.keras.callbacks.TensorBoard(log_dir=args.outputPath+params["log-dir"]+"/"+nn_name, histogram_freq=None, embeddings_freq=None)
 
 
     sgd = tf.keras.optimizers.SGD(lr=params["learning-rate"], momentum=params["momentum"])
@@ -123,5 +125,5 @@ for model in args.models:
 
     print(epsilon)
 
-    with open(args.outputPath+"/trainingTime"+nn_name+".csv", "a+") as fp:
+    with open(args.outputPath+"trainingTime"+nn_name+".csv", "a+") as fp:
         fp.write(args.inputFile+", "+str(elapsed)+", "+str(epsilon)+"\n")
